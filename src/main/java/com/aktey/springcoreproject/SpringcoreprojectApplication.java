1838fcd3d72cd0eff76f46a4a4a5bd78798c01c9
s24377 <s24377@pjwstk.edu.pl>
2023-03-30 21:24:38 +0200

    Initial commit

---
+++ src/main/java/com/aktey/springcoreproject/SpringcoreprojectApplication.java
@@ -0,0 +1,13 @@
+package com.aktey.springcoreproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcoreprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcoreprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aktey/springcoreproject/SpringcoreprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aktey.springcoreproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcoreprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
